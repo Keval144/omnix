08c7c8e9af50f03129b044ed21590ac340535022
keval144 <kevalm144@gmail.com>
2026-03-21 16:04:59 +0530

    Deleting Rewrites

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,14 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [25, 50, 75, 100],
   },
-  rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://omnix-backend-l2h4.onrender.com/:path*",
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
