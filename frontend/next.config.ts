fed778c7a5c9b7298156b674e83d9d7dd876b924
keval144 <kevalm144@gmail.com>
2026-03-21 15:58:25 +0530

    Rewrite Fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [25, 50, 75, 100],
   },
+  rewrites() {
+    return [
+      {
+        source: "/notebook/:path*",
+        destination: "https://omnix-backend-l2h4.onrender.com/notebook/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
